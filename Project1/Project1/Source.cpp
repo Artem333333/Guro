5a0911929ccbb8b6c6819dba67d769d17801ddce
Artem333333 <82451512+Artem333333@users.noreply.github.com>
2022-06-22 15:04:47 +0300

    Add files via upload

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int arr[15], n;
+	cout << "input n";
+	cin >> n;
+	for (int i = 0; i <= n; i++) 
+	{
+		cout << "Arr";
+		cin >> arr[i];
+	}
+	int max = arr[0];
+	for (int i = 0; i <= n; i += 1) {
+		if (max<arr[i])
+
+		{ max = arr[i];
+		}
+	}
+	cout << "Max=" << max << endl;
+
+	return 0;
+}
